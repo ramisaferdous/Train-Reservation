6bb5fe916e563091e9aff2c3b5c3b77a92a5d8fe
ramisaferdous <rifahramisa@iut-dhaka.edu>
2024-01-18 22:10:03 +0600

    added Main Class

---
+++ src/Main.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<String> destinationNames = RandomGenerator.generateRandomDestinations();
+
+        Station stationA = RandomGenerator.createRandomStation(destinationNames);
+        Station stationB = RandomGenerator.createRandomStation(destinationNames);
+        Station stationC = RandomGenerator.createRandomStation(destinationNames);
+        Station stationD = RandomGenerator.createRandomStation(destinationNames);
+        Station stationE = RandomGenerator.createRandomStation(destinationNames);
+
+
+
+        List<Station> routeStations = new ArrayList<>();
+        routeStations.add(stationA);
+        routeStations.add(stationB);
+        routeStations.add(stationC);
+        routeStations.add(stationD);
+        routeStations.add(stationE);
+        Route route = new Route(routeStations);
+
+        System.out.println("Welcome to Railway Management System!");
+        int choice;
+
+        do {
+            System.out.println("\nMain Menu:");
+            System.out.println("1. Create Passenger");
+            System.out.println("2. Login As Passenger");
+            System.out.println("3. Show All Passengers");
+            System.out.println("0. Exit");
+            System.out.print("Enter your choice: ");
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    RailwayManagementSystem.createPassenger(scanner);
+                    break;
+                case 2:
+                    RailwayManagementSystem.loginAndBookTicket(scanner, route);
+                    break;
+                case 3:
+                    RailwayManagementSystem.displayAllPassengers();
+                    break;
+                case 0:
+                    System.out.println("Exiting Railway Management System. Goodbye!");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please enter a valid option.");
+                    break;
+            }
+        } while (choice != 0);
+
+        scanner.close();
+    }
+}
